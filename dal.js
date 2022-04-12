b9551f12772339ae4e07b810bdaff21102563c02
Sherry Boxall <87145748+sherryboxall@users.noreply.github.com>
2022-04-12 14:22:33 -0700

    Update dal.js

--- dal.js
+++ dal.js
@@ -7,7 +7,7 @@ MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
     console.log("Connected successfully to db server");
     console.log(err);
     // connect to myproject database
-   db = client.db('myproject');
+   db = client.db('badbank');
  //  db = heroku config:set MONGODB_URI="mongodb+srv://sboxall:<me12>@badbank.otqja.mongodb.net/client.'badbank'?retryWrites=true&w=majority"
    
 
